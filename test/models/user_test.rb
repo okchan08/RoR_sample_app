5e062918fc145380a49448cbba2fe6be877163f7
okchan08 <yoshiki.o1994@gmail.com>
2019-10-13 22:07:59 +0900

    Add validation to user model

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,61 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = User.new(name: "Example User", email: "user@example.com")
+  end
+
+  test "should be valid" do
+    assert @user.valid?
+  end
+
+  test "name should be present" do
+    @user.name = "   "
+    assert_not @user.valid?
+  end
+
+  test "email should be present" do
+    @user.email = ""
+    assert_not @user.valid?
+  end
+
+  test "name should be less than 50" do
+    @user.name = "a" * 51
+    assert_not @user.valid?
+  end
+
+  test "email should be less than 255" do
+    @user.email = "a" * 244 + "@example.com"
+    assert_not @user.valid?
+  end
+
+  test "email validation rejection" do
+    invalid_addresses = %w[user@example,com user_at_foo.org user.name@example. foo@bar_baz.com foo@bar+baz.com]
+    invalid_addresses.each do |invalid_address|
+      @user.email = invalid_address
+      assert_not @user.valid?, "#{invalid_address.inspect} should be invalid"
+    end
+  end
+
+  test "email validation accept" do
+    valid_addresses = %w[user@example.com user@foo.org name@example.org]
+    valid_addresses.each do |valid_address|
+      @user.email = valid_address
+      assert @user.valid?, "#{valid_address.inspect} should be invalid"
+    end
+  end
+
+  test "email address should be unique" do
+    duplicate_user = @user.dup
+    duplicate_user.email = @user.email.upcase
+    @user.save
+    assert_not duplicate_user.valid?
+  end
+
+  test "email should be saved as lower-case" do
+    test_email = "Foo@ExAMPle.CoM"
+    @user.email = test_email
+    @user.save
+    assert_equal test_email.downcase, @user.reload.email
+  end
 end
