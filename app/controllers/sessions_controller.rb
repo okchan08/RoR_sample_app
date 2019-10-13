e11b6d879e6295163463a870ee6796cef4fbb6d0
okchan08 <yoshiki.o1994@gmail.com>
2019-10-13 23:43:35 +0900

    Add login page and some layouts

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,20 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+
+    if user && user.authenticate(params[:session][:password])
+      log_in user
+      flash[:success] = "Login successful!"
+      redirect_to user
+    else
+      flash.now[:danger] = "Invalid login information"
+      render 'new'
+    end
+  end
+
+  def destroy
+  end
 end
