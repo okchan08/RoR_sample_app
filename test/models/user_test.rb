ed878b4b1d92cbd5461fccf3f8c17096de7a83b8
okchan08 <yoshiki.o1994@gmail.com>
2019-10-13 22:16:56 +0900

    Add password validation

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -58,4 +58,14 @@ class UserTest < ActiveSupport::TestCase
     @user.save
     assert_equal test_email.downcase, @user.reload.email
   end
+
+  test "password should be present" do
+    @user.password = @user.password_confirmation = " " * 6
+    assert_not @user.valid?
+  end
+
+  test "password should have a minimum length" do
+    @user.password = @user.password_confirmation = "12345"
+    assert_not @user.valid?
+  end
 end
