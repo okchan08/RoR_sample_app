343446186f3047f6ce35522b272246746125b9b1
okchan08 <yoshiki.o1994@gmail.com>
2019-10-14 11:16:43 +0900

    Fix some bugs for cookies

--- app/models/user.rb
+++ app/models/user.rb
@@ -28,6 +28,7 @@ class User < ApplicationRecord
 
   # return true if given token matches with digest
   def authenticated?(remember_token)
+    return false if remember_digest.nil?
     BCrypt::Password.new(remember_digest).is_password?(remember_token)
   end
 
--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -38,6 +38,8 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     delete logout_path
     assert_not is_logged_in?
     assert_redirected_to root_url
+    # simulate clicking logout button in second browser
+    delete logout_path
     follow_redirect!
     assert_select "a[href=?]", login_path
     assert_select "a[href=?]", logout_path, count: 0
