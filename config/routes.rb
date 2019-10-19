8a1a5a4dc3346367a88a7d22cf4282fa4ac12f89
okchan08 <yoshiki.o1994@gmail.com>
2019-10-19 15:13:24 +0900

    Add reset_digest to users

--- config/routes.rb
+++ config/routes.rb
@@ -16,6 +16,7 @@ Rails.application.routes.draw do
 
   resources :users
   resources :account_activations, only: [:edit]
+  resources :password_resets, only: [:new, :create, :edit, :update]
 
   if Rails.env.development?
     mount LetterOpenerWeb::Engine, at: "/letter_opener"
